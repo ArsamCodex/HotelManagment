7b0ede952fdf671b75f18091c242e710b8a1aa10
ArsaMUK <ArsaM@DESKTOP-DT26U4I>
2024-04-20 22:31:30 +0100

    some validation added

--- Shared/RoomInspection.cs
+++ Shared/RoomInspection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,10 @@ namespace HotelManagment.Shared
     public class RoomInspection
     {
         public int RoomInspectionID { get; set; }
-        public int RoomNumber { get; set; }
+        [Required(ErrorMessage = "RoomNumber is required")]
+        public int? RoomNumber { get; set; }
         public DateTime? InspectionDate { get; set; }
+        [Required(ErrorMessage = "ProblemDescription is required")]
         public string? ProblemDescription { get; set; }
         public string Staff { get; set; }
       //  public int? RoomID { get; set; }
